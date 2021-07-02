8d9da44691f8979a1515b9375935152f6e0f3e88
LEONIDAS <jesus.leon1@unmsm.edu.pe>
2021-07-02 18:28:03 -0500

    ArregloLoginRegister

--- packages/client/src/pages/PageOne.jsx
+++ packages/client/src/pages/PageOne.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../styles/register.css'
 import VIcon from '../components/VIcon/Logo'
-import { Box, Text, FormControl, FormLabel, Input, Button, Image } from "@chakra-ui/react"
+import { Box, Text, FormControl, FormLabel, Input, Button, Image, Link } from "@chakra-ui/react"
 import { useForm } from '../hooks/useForm'
 
 export default function PageOne() {
@@ -18,75 +18,80 @@ export default function PageOne() {
                 </div>
                 <div className="ConteinerForm">
                     <Box ml="50" width="400px">
-                            <Text
-                            color="#525252"
-                            fontFamily="Poppins"
-                            fontStyle="normal"
-                            fontSize="18px"
-                            p="10">Registrarse</Text>
-                            <VIcon ></VIcon>
-                            <Text
-                            color="#9C94F2"
-                            fontFamily="Poppins"
-                            fontStyle="normal"
-                            fontSize="30px"
-                            p="1">Inicio de sesión</Text>
-                            <Text
-                            color="#525252"
-                            fontFamily="Poppins"
-                            fontStyle="normal"
-                            fontSize="20px"
-                            p="1">¡Bienvenido otra vez a Eventimor!</Text>
-                            <form onSubmit={handleSubmit}>
-                                <FormControl>
-                                    <FormLabel
-                                    color="#525252"
-                                    fontFamily="Poppins"
-                                    fontStyle="normal"
-                                    fontSize="18px"
-                                    p="3">Correo electrónico</FormLabel>
-                                    <Input borderColor="Black"
-                                    _hover={{
-                                        color: "black",
-                                      }}
-                                    type="email"
-                                    onChange={handleInputChange}
-                                    value={email}
-                                    name="email"
-                                    />
-                                </FormControl>
-                                <FormControl>
-                                    <FormLabel
-                                    color="#525252"
-                                    fontFamily="Poppins"
-                                    fontStyle="normal"
-                                    fontSize="18px"
-                                    p="3">Contraseña</FormLabel>
-                                    <Input borderColor="Black"
-                                    _hover={{
-                                        color: "black",
-                                      }}
-                                    type="password"
-                                    onChange={handleInputChange}
-                                    value={password}
-                                    name="password"
-                                    />
-                                </FormControl>
-                                <Box pt="10">
-                                    <Button
-                                    colorScheme="#5D467C"
-                                    textAlign="left"
-                                    w="400px"
-                                    bg="#A179D9"
-                                    color="#FFFFFF"
-                                    fontFamily="Poppins"
-                                    fontStyle="normal"
-                                    fontSize="20px"
-                                    borderRadius="7px"
-                                    type="submit"
-                                    >Iniciar Sesión</Button>
-                                </Box>
-                            </form>
+                            <Box p="20" ml="15">
+                                <Link
+                                color="#525252"
+                                fontFamily="Poppins"
+                                fontStyle="normal"
+                                fontSize="18px"
+                                p="10"
+                                href="register">Registrarse</Link>
+                            </Box>
+                            <Box mt="30">
+                                <VIcon ></VIcon>
+                                <Text
+                                color="#9C94F2"
+                                fontFamily="Poppins"
+                                fontStyle="normal"
+                                fontSize="30px"
+                                p="1">Inicio de sesión</Text>
+                                <Text
+                                color="#525252"
+                                fontFamily="Poppins"
+                                fontStyle="normal"
+                                fontSize="20px"
+                                p="1">¡Bienvenido otra vez a Eventimor!</Text>
+                                <form onSubmit={handleSubmit}>
+                                    <FormControl>
+                                        <FormLabel
+                                        color="#525252"
+                                        fontFamily="Poppins"
+                                        fontStyle="normal"
+                                        fontSize="18px"
+                                        p="3">Correo electrónico</FormLabel>
+                                        <Input borderColor="Black"
+                                        _hover={{
+                                            color: "black",
+                                        }}
+                                        type="email"
+                                        onChange={handleInputChange}
+                                        value={email}
+                                        name="email"
+                                        />
+                                    </FormControl>
+                                    <FormControl>
+                                        <FormLabel
+                                        color="#525252"
+                                        fontFamily="Poppins"
+                                        fontStyle="normal"
+                                        fontSize="18px"
+                                        p="3">Contraseña</FormLabel>
+                                        <Input borderColor="Black"
+                                        _hover={{
+                                            color: "black",
+                                        }}
+                                        type="password"
+                                        onChange={handleInputChange}
+                                        value={password}
+                                        name="password"
+                                        />
+                                    </FormControl>
+                                    <Box pt="10">
+                                        <Button
+                                        colorScheme="#5D467C"
+                                        textAlign="left"
+                                        w="400px"
+                                        bg="#A179D9"
+                                        color="#FFFFFF"
+                                        fontFamily="Poppins"
+                                        fontStyle="normal"
+                                        fontSize="20px"
+                                        borderRadius="7px"
+                                        type="submit"
+                                        >Iniciar Sesión</Button>
+                                    </Box>
+                                </form>
+                            </Box>
                         </Box>
                 </div>
         </div>
